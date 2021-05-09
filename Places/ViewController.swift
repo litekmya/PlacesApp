2a09fa7589d37c930429f39a4e7bd47c5dca201e
Vladimir Li <tekmya@mail.ru>
2021-05-09 13:41:50 +0900

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Владимир Ли on 09.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
